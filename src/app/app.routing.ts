c6c00796d5c0fdb54b8a14fc474317b2119448dd
nebeinstein <nebeinstein@wpi.edu>
2017-02-11 22:33:50 -0500

    fixed the routing for recipes cuz we're idiots

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,12 @@
 import { Routes, RouterModule } from '@angular/router';
 
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 const APP_ROUTES: Routes = [
 	{ path: 'shopping-list', component: ShoppingListComponent },
-	{ path: 'recipes', component: RecipeListComponent },
-	{ path: '', component: RecipeListComponent }
+	{ path: 'recipes', component: RecipesComponent },
+	{ path: '', component: RecipesComponent }
 ];
 
 export const Routing = RouterModule.forRoot(APP_ROUTES);
