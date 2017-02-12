d759df3b8fdef1150d1e309f5af2b715171f6a79
nebeinstein <nebeinstein@wpi.edu>
2017-02-11 19:25:09 -0500

    changed website title from "RecipeBook" to "Recipe Book" because it was bothering me

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ShoppingListAddComponent } from './shopping-list/shopping-list-add.comp
 import { DropdownDirective } from './dropdown.directive';
 import { RecipeService } from './recipes/recipe.service';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -31,7 +32,10 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
     FormsModule,
     HttpModule
   ],
-  providers: [RecipeService, ShoppingListService],
+  providers: [
+    RecipeService,
+    ShoppingListService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
